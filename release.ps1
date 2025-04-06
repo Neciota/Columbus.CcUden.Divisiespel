0c081840b80d5660c464b9475ee98162b6a849ec
Neciota <rvdberg-@hotmail.com>
2025-04-06 10:47:11 +0200

    Restore for x86

--- release.ps1
+++ release.ps1
@@ -39,7 +39,7 @@ if (Test-Path $outDir) {
 Push-Location $projDir
 try {
     Write-Output "Restoring:"
-    dotnet restore -r win-x64
+    dotnet restore -r win-x86
     Write-Output "Publishing:"
     $msBuildVerbosityArg = "/v:m"
     if ($env:CI) {
